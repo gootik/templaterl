f8654a65ee70fb16bb301436e6afb3101443a30c
Sasan Hezarkhani <sasan.hezarkhani@vungle.com>
2016-09-13 12:54:21 -0700

    Creates a module with the fun info

--- src/templaterl.erl
+++ src/templaterl.erl
@@ -18,12 +18,14 @@
 -compile(inline).
 
 -export([
-    compile/2
+    compile/2,
+    register_helpers/1
 ]).
 
 -type token_list() :: [token() | token_expression()].
 -type token() :: {bitstring(), bitstring()}.
--type token_expression() :: {bitstring(), function()}.
+-type token_expression() :: {bitstring(), token_helper_function()}.
+-type token_helper_function() :: fun((bitstring(), bitstring()) -> bitstring()).
 
 %%====================================================================
 %% API functions
@@ -31,6 +33,37 @@
 -spec compile(bitstring(), token_list()) -> bitstring().
 compile(Bin, Tokens) when is_bitstring(Bin) andalso is_list(Tokens) ->
     parse_and_replace(Bin, Tokens, <<>>).
+
+-spec register_helpers(list(token_expression())) -> ok.
+register_helpers(HelperList) ->
+    try
+        Forms = helper_forms(HelperList),
+        io:format(user, "~p~n", [Forms]),
+        {ok, Module, Bin} = compile:forms(Forms, [debug_info]),
+        code:purge(Module),
+        Filename = atom_to_list(Module) ++ ".erl",
+        {module, Module} = code:load_binary(Module, Filename, Bin),
+        ok
+    catch Error:Reason ->
+        io:format(user, "~p:~p:~p ~n", [Error, Reason, erlang:get_stacktrace()])
+    end.
+
+helper_forms(HelperList) ->
+    ModuleDefinition = erl_syntax:attribute(erl_syntax:atom(module), [erl_syntax:atom(templaterl_helpers)]),
+    ExportsList = [erl_syntax:arity_qualifier(erl_syntax:atom(HelperName), erl_syntax:integer(2)) || HelperName <- proplists:get_keys(HelperList)],
+    Export = erl_syntax:attribute(erl_syntax:atom(export), [erl_syntax:list(ExportsList)]),
+
+    Functions = [erl_syntax:function(erl_syntax:atom(HelperName), helper_function_clauses(HelperName, HelperBody)) || {HelperName, HelperBody} <- HelperList],
+
+    Module = [ModuleDefinition, Export] ++ Functions,
+    [erl_syntax:revert(X) || X <- Module].
+
+helper_function_clauses(_HelperName, HelperBody) ->
+    io:format(user, "~p~n", [HelperBody]),
+    io:format(user, "~p~n", [erlang:fun_info(HelperBody)]),
+    {env, [{_,_,_,Forms}]} = erlang:fun_info(HelperBody, env),
+
+    Forms.
 
 %%====================================================================
 %% Internal functions
@@ -69,8 +102,7 @@ apply_token_funs(TokenBin, Tokens) ->
             {_, Value} = lists:keyfind(Token, 1, Tokens),
             lists:foldl(
                 fun(Current, Prev) ->
-                    {_, Fun} = lists:keyfind(Current, 1, Tokens),
-                    Fun(Token, Prev)
+                    apply(templaterl_helpers, binary_to_existing_atom(Current, utf8), [Token, Prev])
                 end,
                 Value,
                 Funs)
