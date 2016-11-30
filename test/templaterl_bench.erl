9ccd55f5b45007d63e9a534e81163b724b4bfc36
Sasan Hezarkhani <sasan.hezarkhani@vungle.com>
2016-11-30 15:32:41 +0000

    Adds a better profiling method, without measuring other libraries. Adds a new benchmarking test

--- test/templaterl_bench.erl
+++ test/templaterl_bench.erl
@@ -34,10 +34,44 @@ run() ->
                                 {<<"sollicitudin">>, <<"REPLACE8">>}])
         end, 100000, 20),
 
+    %% We use a dummy fun because we are just measuring templaterl not the custom
+    %% function's performance.
+    DummyFun = "lowercase(_, Value) -> Value.",
+
+    templaterl:register_helpers([DummyFun]),
+
+    LongWithFunsTiming = timing:function(
+        fun() ->
+            templaterl:compile(<<"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque mollis",
+                                 "nunc non turpis scelerisque {{{lowercase vestibulum}}}. Vestibulum ac nisl sit amet quam",
+                                 "{{{convallis}}} pretium vitae ut diam. Nunc at nisl consequat, {{{feugiat}}} arcu vel,",
+                                 "eleifend magna. Nulla in justo nibh. Morbi ac leo eget purus ornare sodales",
+                                 "quis non orci. Sed et {{{lorem}}} vel est efficitur tincidunt non a ante.",
+                                 "Duis lobortis ipsum eu suscipit posuere. Interdum et malesuada fames ac ante",
+                                 "ipsum primis in faucibus. Morbi {{{efficitur}}} lorem ut est {{{congue}}} rhoncus. Aenean",
+                                 "sodales lobortis erat, eu facilisis massa varius at. In rhoncus lacus ut turpis",
+                                 "pretium, vel convallis augue consequat. Vestibulum hendrerit ultrices est sit",
+                                 "amet {{{pellentesque}}}. Sed et turpis vitae dui ultrices pellentesque nec ut justo.",
+                                 "Nullam vitae sem congue, molestie neque a, hendrerit eros. Pellentesque",
+                                 "{{{lowercase (sollicitudin)}}} eu lorem a malesuada">>,
+                               [{<<"vestibulum">>, <<"REPLACE1">>},
+                                {<<"convallis">>, <<"REPLACE2">>},
+                                {<<"feugiat">>, <<"REPLACE3">>},
+                                {<<"lorem">>, <<"REPLACE4">>},
+                                {<<"efficitur">>, <<"REPLACE5">>},
+                                {<<"congue">>, <<"REPLACE6">>},
+                                {<<"pellentesque">>, <<"REPLACE7">>},
+                                {<<"sollicitudin">>, <<"REPLACE8">>}])
+        end, 100000, 20),
+
     io:format("Small text~n"),
     io:format("====================================~n"),
     io:format("~p~n", [SmallTiming]),
 
     io:format("~nLong text~n"),
     io:format("====================================~n"),
-    io:format("~p~n", [LongTiming]).
+    io:format("~p~n", [LongTiming]),
+
+    io:format("~nLong text with custom functions~n"),
+    io:format("====================================~n"),
+    io:format("~p~n", [LongWithFunsTiming]).
